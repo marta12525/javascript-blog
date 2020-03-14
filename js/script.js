a471ce659514d418b1eaacb609322f784c2a2e3f
Marta Bednarz <martab2525@gmail.com>
2020-03-14 12:35:35 +0100

    remove class active from all article links and articles

--- js/script.js
+++ js/script.js
@@ -5,15 +5,25 @@ document.getElementById('test-button').addEventListener('click', function(){
   });
   */
 
- const titleClickHandler = function(event){
+ const titleClickHandler = function (event) {
     console.log('Link was clicked!');
     console.log(event);
   
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }  
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
@@ -24,6 +34,6 @@ document.getElementById('test-button').addEventListener('click', function(){
   
   const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
+  for (let link of links) {
     link.addEventListener('click', titleClickHandler);
   }
