d65fd65db624239a0fc0a52cf761d6f7f14f1350
Marta Bednarz <martab2525@gmail.com>
2020-03-19 17:14:36 +0100

    fix spaces

--- js/script.js
+++ js/script.js
@@ -1,71 +1,67 @@
 'use strict';
 {
-function clearMessages (list) {
-	list.innerHTML = '';
-}
-
- const titleClickHandler = function (event) {
-	event.preventDefault();
-	const clickedElement = this;
-  
-	const activeLinks = document.querySelectorAll('.titles a.active');
-
-	for (let activeLink of activeLinks) {
-		activeLink.classList.remove('active');
-	}  
-  
-	clickedElement.classList.add('active');
-
-	const activeArticles = document.querySelectorAll('.posts article.active');
-
-	for (let activeArticle of activeArticles) {
-		activeArticle.classList.remove('active');
-	}
-
-	const articleSelector = clickedElement.getAttribute('href');
-  
-	const correctArticle = document.querySelector(articleSelector);
-
-	correctArticle.classList.add('active');
+  function clearMessages (list) {
+    list.innerHTML = '';
   }
 
+  const titleClickHandler = function (event) {
+    event.preventDefault();
+    const clickedElement = this;
+  
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove('active');
+    }  
+  
+    clickedElement.classList.add('active');
+
+    const activeArticles = document.querySelectorAll('.posts article.active');
+
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active');
+    }
+
+    const articleSelector = clickedElement.getAttribute('href');
+  
+    const correctArticle = document.querySelector(articleSelector);
+
+    correctArticle.classList.add('active');
+
+  };
+
   const optArticleSelector = '.post',
-	optTitleSelector = '.post-title',
-	optTitleListSelector = '.titles';
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
   function generateTitleLinks () {
 
-	const titleList = document.querySelector(optTitleListSelector);
+    const titleList = document.querySelector(optTitleListSelector);
 
-	clearMessages(titleList);
+    clearMessages(titleList);
 
-	const articles = document.querySelectorAll(optArticleSelector);
+    const articles = document.querySelectorAll(optArticleSelector);
 
-	let html = '';
+    let html = '';
 
-	for (let article of articles) {
-	  	const articleId = article.getAttribute('id');
+    for (let article of articles) {
+      const articleId = article.getAttribute('id');
 
-	  	const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
-	 	const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
 
-		html = html + linkHTML;
-	}
+      html = html + linkHTML;
+    }
 
-	titleList.innerHTML = html;
+    titleList.innerHTML = html;
 
-	const links = document.querySelectorAll('.titles a');
-  
-	for (let link of links) {
-	  link.addEventListener('click', titleClickHandler);
-	}
+    const links = document.querySelectorAll('.titles a');
+
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
 
   }
-
-generateTitleLinks();
-
-
-
-
+  generateTitleLinks();
 }
